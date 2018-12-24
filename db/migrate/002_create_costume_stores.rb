ce84896b3f4f65a03efa943aa5192339c4d5398c
Dan Hintz <dhintz@live.com>
2018-12-24 23:01:20 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -5,7 +5,7 @@ class CreateConstumeStores < ActiveRecord::Migration[4.2]
       t.string :name
       t.string :location
       t.integer :costume_inventory
-      t.integer :number_of_employees
+      t.integer :employees_count
       t.boolean :in_business
       t.datetime :opening_time
       t.datetime :closing_time
